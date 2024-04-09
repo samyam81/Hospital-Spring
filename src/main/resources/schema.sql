6a91cd945fb608da1e13edc92a70e87a1e46fcad
samyam81 <samyam081@gmail.com>
2024-04-09 15:11:37 +0545

    Changed

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE Hospital (
+    hospital_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    address VARCHAR(255),
+    number VARCHAR(20)
+);
+
+CREATE TABLE Nurse (
+    nurse_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    hospital_id INT,
+    FOREIGN KEY (hospital_id) REFERENCES Hospital(hospital_id)
+);
+
+CREATE TABLE Doctor (
+    doctor_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    hospital_id INT,
+    FOREIGN KEY (hospital_id) REFERENCES Hospital(hospital_id)
+);
+
+CREATE TABLE Patient (
+    patient_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    address VARCHAR(255)
+);
+
+CREATE TABLE Treatment (
+    treatment_id INT PRIMARY KEY,
+    date DATE,
+    appointment_id INT,
+    FOREIGN KEY (appointment_id) REFERENCES Appointment(appointment_id)
+);
+
+CREATE TABLE Appointment (
+    appointment_id INT PRIMARY KEY,
+    date DATE,
+    doctor_id INT,
+    patient_id INT,
+    FOREIGN KEY (doctor_id) REFERENCES Doctor(doctor_id),
+    FOREIGN KEY (patient_id) REFERENCES Patient(patient_id)
+);
