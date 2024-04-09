f212349bc55f1b9903da4fdda5299d4f9fefa5d8
samyam81 <samyam081@gmail.com>
2024-04-09 16:16:45 +0545

    Last for day

--- src/main/java/com/hospital/hospital/HospitalApplication.java
+++ src/main/java/com/hospital/hospital/HospitalApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.hospital.hospital.Entity.Appointment;
 import com.hospital.hospital.Entity.Doctor;
 import com.hospital.hospital.Entity.Nurse;
 import com.hospital.hospital.Entity.Patient;
+import com.hospital.hospital.Entity.Treatment;
 
 @SpringBootApplication
 public class HospitalApplication {
@@ -26,6 +28,13 @@ public class HospitalApplication {
 		Nurse n1= context.getBean(Nurse.class);
 		n1.setNurse_id(1);
 		n1.setName("Nurse Smith");
+
+		Treatment t1=context.getBean(Treatment.class);
+		t1.setTreatment_Id(1);
+		t1.setDate("2024-04-10");
+
+		Appointment a1=context.getBean(Appointment.class);
+		a1.setAppointment_ID(1);
 
 	}
 }
