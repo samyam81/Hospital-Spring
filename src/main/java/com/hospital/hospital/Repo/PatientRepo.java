2c6f23a27ea697dac7b1802fe005ade575a75914
samyam81 <samyam081@gmail.com>
2024-04-10 11:45:01 +0545

    Commit

--- src/main/java/com/hospital/hospital/Repo/PatientRepo.java
+++ src/main/java/com/hospital/hospital/Repo/PatientRepo.java
@@ -1,7 +1,12 @@
 package com.hospital.hospital.Repo;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.hospital.hospital.Entity.Patient;
@@ -12,18 +17,26 @@ public class PatientRepo {
     @Autowired
     private JdbcTemplate template;
 
-    public JdbcTemplate getTemplate() {
-        return template;
-    }
-
-    public void setTemplate(JdbcTemplate template) {
-        this.template = template;
-    }
-
     public void save(Patient p1){
-        String sql="Insert into Patient(id,name,Address)& values(?,?,?)";
-        int rows=template.update(sql,p1.getPatientID(),p1.getName(),p1.getAddress());
-        System.out.println("Rows Affected:::"+rows);    
+        String sql = "INSERT INTO Patient (id, name, Address) VALUES (?, ?, ?)";
+        int rows = template.update(sql, p1.getPatientID(), p1.getName(), p1.getAddress());
+        System.out.println("Rows Affected: " + rows);    
     }
 
+    public List<Patient> findAll(){
+        String sql = "SELECT * FROM p1";
+        List<Patient> patients = template.query(sql, new PatientRowMapper());
+        return patients;
+    }
+
+    private class PatientRowMapper implements RowMapper<Patient> {
+        @Override
+        public Patient mapRow(ResultSet rs, int rowNum) throws SQLException{
+            Patient p1 = new Patient();
+            p1.setPatientID(rs.getInt("id"));
+            p1.setName(rs.getString("name"));
+            p1.setAddress(rs.getString("Address"));
+            return p1;
+        }
+    }
 }
