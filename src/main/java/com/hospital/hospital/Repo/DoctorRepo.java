1d9a6404fdab0608b9affae4a51d18f5070b1c80
samyam81 <samyam081@gmail.com>
2024-04-10 11:53:30 +0545

    Edited

--- src/main/java/com/hospital/hospital/Repo/DoctorRepo.java
+++ src/main/java/com/hospital/hospital/Repo/DoctorRepo.java
@@ -1,9 +1,14 @@
 package com.hospital.hospital.Repo;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.swing.tree.RowMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
 import com.hospital.hospital.Entity.Doctor;
 
 @Repository
@@ -20,12 +25,29 @@ public class DoctorRepo {
         this.template = template;
     }
 
-    // Change this into Doctor class
+
     public void save(Doctor d1){
         String sql="Insert into Doctor(id,name)& values(?,?)";
         int rows=template.update(sql,d1.getDoctor_id(),d1.getName());
         System.out.println("Rows Affected:::"+rows);    
-    } //Done.
+    }
 
+    public List<Doctor> findAll(){
+        String sql = "SELECT * FROM d1";
+        List<Doctor> doctors = template.query(sql, new DoctorRowMapper());
+        return doctors;
+    }
+
+    private class DoctorRowMapper implements RowMapper<Doctor> {
+        @Override
+        public Doctor mapRow(ResultSet rs, int rowNum) throws SQLException{
+         Doctor d1=new Doctor();
+         d1.setDoctor_id(rs.getInt("id"));
+         d1.setName(rs.getNString("Name"));
+        return d1;
+        }
+    
+        
+    }
 
 }
