c82f8b19fe924a5ec96876b51ec208a47a199715
samyam81 <samyam081@gmail.com>
2024-04-10 12:01:17 +0545

    Commit

--- src/main/java/com/hospital/hospital/Repo/AppointmentRepo.java
+++ src/main/java/com/hospital/hospital/Repo/AppointmentRepo.java
@@ -1,27 +1,41 @@
 package com.hospital.hospital.Repo;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.hospital.hospital.Entity.Appointment;
 
 @Repository
 public class AppointmentRepo {
+    
     @Autowired
     private JdbcTemplate template;
 
-    public JdbcTemplate getTemplate() {
-        return template;
+    public void save(Appointment a1) {
+        String sql = "INSERT INTO Appointment (id) VALUES (?)";
+        int rows = template.update(sql, a1.getAppointment_ID());
+        System.out.println("Rows Affected: " + rows);
     }
 
-    public void setTemplate(JdbcTemplate template) {
-        this.template = template;
+    public List<Appointment> findAll() {
+        String sql = "SELECT * FROM Appointment";
+        List<Appointment> appointments = template.query(sql, new AppointmentRowMapper());
+        return appointments;
     }
 
-    public void save(Appointment a1){
-        String sql="Insert into Appointment(id)& values(?)";
-        int rows=template.update(sql,a1.getAppointment_ID());
-        System.out.println("Rows Affected:::"+rows);   
+    private class AppointmentRowMapper implements RowMapper<Appointment> {
+        @Override
+        public Appointment mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Appointment appointment = new Appointment();
+            appointment.setAppointment_ID(rs.getInt("id"));
+            // Set other properties of the Appointment object if necessary
+            return appointment;
+        }
     }
 }
